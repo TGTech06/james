ac873cceb4c042ccda7a6edad64728fd97042b2c
TGTech06 <tgtech06@gmail.com>
2023-07-24 23:16:29 +0100

    Redesigned intro screens

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,6 +6,8 @@
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZmFuZGN4Y2V6dGVidHB3enhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNzI0NzUsImV4cCI6MjAwNDk0ODQ3NX0.MXs4u_1XMM-foNe08LLYHQLENjmwTF3jqUmNXCSbOU4"
   );
 
+  let errorMessage = "";
+
   const loginUser = async (event: Event) => {
     event.preventDefault();
 
@@ -18,22 +20,59 @@
     });
 
     if (error) {
-      console.log(error);
-      return;
+      errorMessage = error.message;
+    } else {
+      errorMessage = ""; // Clear the error message if there was no error
+      console.log(data?.session?.user);
+      window.location.href = "/";
     }
+  };
 
-    console.log(data?.session?.user);
-    window.location.href = "/";
+  const toggleView = () => {
+    window.location.href = "/register"; // Redirect to the register page
   };
 </script>
 
-<main>
-  <h1>Login</h1>
-  <form on:submit|preventDefault={loginUser} class="auth-form">
-    <label for="">Email</label>
-    <input type="text" name="email" />
-    <label for="">Password</label>
-    <input type="password" name="password" />
-    <button class="btn btn-primary">Login</button>
-  </form>
+<main class="min-h-screen flex items-center justify-center bg-gray-800">
+  <div class="bg-gray-900 p-8 rounded-lg shadow-md w-full max-w-md">
+    <h1 class="text-3xl font-semibold text-white text-center mb-6">Login</h1>
+    <form on:submit|preventDefault={loginUser}>
+      <div class="mb-4">
+        <label for="email" class="block text-gray-300 text-sm font-bold"
+          >Email</label
+        >
+        <input
+          type="text"
+          name="email"
+          class="form-input mt-1 block w-full py-2 px-4 rounded-md bg-gray-700 text-white"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="password" class="block text-gray-300 text-sm font-bold"
+          >Password</label
+        >
+        <input
+          type="password"
+          name="password"
+          class="form-input mt-1 block w-full py-2 px-4 rounded-md bg-gray-700 text-white"
+        />
+      </div>
+      <button class="btn btn-primary w-full py-3 rounded-lg">Login</button>
+      <div class="mt-4 text-gray-300 text-sm text-center">
+        Don't have an account? <a
+          href="#"
+          on:click={toggleView}
+          class="underline">Register</a
+        >
+      </div>
+    </form>
+  </div>
 </main>
+
+<style>
+  /* Custom styles for centering the form */
+  main {
+    display: flex;
+    align-items: center;
+  }
+</style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,6 +3,8 @@
   import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
   const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
 
+  let errorMessage = "";
+
   const createUser = async (event: Event) => {
     event.preventDefault();
 
@@ -15,22 +17,59 @@
     });
 
     if (error) {
-      console.log(error);
-      return;
+      errorMessage = error.message;
+    } else {
+      errorMessage = ""; // Clear the error message if there was no error
+      console.log(data.user.id);
+      window.location.href = "/";
     }
+  };
 
-    console.log(data.user.id);
-    window.location.href = "/";
+  const toggleView = () => {
+    window.location.href = "/login"; // Redirect to the login page
   };
 </script>
 
-<main>
-  <h1>Register</h1>
-  <form on:submit|preventDefault={createUser} class="auth-form">
-    <label for="">Email</label>
-    <input type="text" name="email" />
-    <label for="">Password</label>
-    <input type="password" name="password" />
-    <button class="btn btn-primary">Register</button>
-  </form>
+<main class="min-h-screen flex items-center justify-center bg-gray-800">
+  <div class="bg-gray-900 p-8 rounded-lg shadow-md w-full max-w-md">
+    <h1 class="text-3xl font-semibold text-white text-center mb-6">Register</h1>
+    <form on:submit|preventDefault={createUser}>
+      <div class="mb-4">
+        <label for="email" class="block text-gray-300 text-sm font-bold"
+          >Email</label
+        >
+        <input
+          type="text"
+          name="email"
+          class="form-input mt-1 block w-full py-2 px-4 rounded-md bg-gray-700 text-white"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="password" class="block text-gray-300 text-sm font-bold"
+          >Password</label
+        >
+        <input
+          type="password"
+          name="password"
+          class="form-input mt-1 block w-full py-2 px-4 rounded-md bg-gray-700 text-white"
+        />
+      </div>
+      <button class="btn btn-primary w-full py-3 rounded-lg">Register</button>
+      <div class="mt-4 text-gray-300 text-sm text-center">
+        Already have an account? <a
+          href="#"
+          on:click={toggleView}
+          class="underline">Login</a
+        >
+      </div>
+    </form>
+  </div>
 </main>
+
+<style>
+  /* Custom styles for centering the form */
+  main {
+    display: flex;
+    align-items: center;
+  }
+</style>
