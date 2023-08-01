be0d8431831ce16c132d270c9cfd20e4f71df1ba
TGTech06 <tgtech06@gmail.com>
2023-08-01 22:23:48 +0200

    Normal adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -17,15 +17,15 @@ import type { Session } from "@supabase/supabase-js"
 declare global {
 	declare namespace App {
 		// interface Error {}
-		interface Platform {
-			env: {
-				COUNTER: DurableObjectNamespace;
-			};
-			context: {
-				waitUntil(promise: Promise<any>): void;
-			};
-			caches: CacheStorage & {default: Cache}
-		}
+		// interface Platform {
+		// 	env: {
+		// 		COUNTER: DurableObjectNamespace;
+		// 	};
+		// 	context: {
+		// 		waitUntil(promise: Promise<any>): void;
+		// 	};
+		// 	caches: CacheStorage & {default: Cache}
+		// }
 		interface Locals {
 			sb: TypedSupabaseClient
 			session: Session | null
